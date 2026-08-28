a916dcfa054658c3fae02435d102eb013a052015
sadiyaumaima03 <sadiyaumaima8@gmail.com>
2026-08-28 17:37:29 +0530

    Operators-day1

---
+++ day1/operators.py
@@ -0,0 +1,11 @@
+a = 3
+b = 4
+
+#Performing operations on a, b operands
+print(a+b) #Addition 
+print(a-b) #Difference
+print(a*b) #Multiplication
+print(a%b) #Modulus
+print(a/b) #Division
+print(a**b) #Exponential
+print(a//b) #Float Division
