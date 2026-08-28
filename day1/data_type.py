316bc46939a66a47805a3ca0ae83ed7eda9dcc44
sadiyaumaima03 <sadiyaumaima8@gmail.com>
2026-08-28 18:09:22 +0530

    Data types

---
+++ day1/data_type.py
@@ -0,0 +1,6 @@
+print(type(3.14))
+print(type(10))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("tom"))
+print(type("Slovenia"))
---
+++ day1/eg_type.py
@@ -0,0 +1,18 @@
+#example of string
+"Tom"
+#example of boolean
+is_slovenian = True
+#example of list, stores different data types
+['Tom', 'slovenian', 'ginger', 'tacos', 16]
+#example of dictionary, stores value in key:value pair
+{
+    "name" : "Tom",
+    "age" : 16,
+    "Country" : "Slovenia",
+    "Food" : "Tacos",
+    "Planet" : "Earth"
+}
+#example of tuple, unlike list, tuple cannot be modified once they are created
+('Earth', 'Venus', 'Jupiter', 'Mercury', 'Saturn')
+#example of set; unlike tup or list, set is not an ordered collection
+{1, 3, 2, 4, 66, 100}
