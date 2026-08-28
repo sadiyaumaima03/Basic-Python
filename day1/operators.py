cb4d108e15e42186bf34654a157fdcca477e1b41
sadiyaumaima03 <sadiyaumaima8@gmail.com>
2026-08-28 18:19:55 +0530

    Add modification of comments

--- day1/operators.py
+++ day1/operators.py
@@ -7,5 +7,6 @@ print(a-b) #Difference
 print(a*b) #Multiplication
 print(a%b) #Modulus
 print(a/b) #Division
-print(a**b) #Exponential
-print(a//b) #Floor Division
+print(a**b) #Exponential; multiplies the number by itself as many times mentioned,
+            #eg: print(3 ** 4) multiplies 3 by itself four times. 
+print(a//b) #Floor Division; divides then rounds to the nearest whole number
